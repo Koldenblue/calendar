53206bf8ffdb905423367ff64cb43f16a286d19d
Kevin Miller <kmillergit@outlook.com>
2020-12-18 23:46:43 -0800

    mini-calendar updates month title properly

--- client/src/components/MiniCalendar.js
+++ client/src/components/MiniCalendar.js
@@ -14,17 +14,10 @@ export default function MiniCalendar() {
 
 
   useEffect(() => {
-    // in order to change the month, we most change both month and year
-    console.log(currentDate);
-    console.log(currentMonth);
-
-    // logs the 1st day of the month plus offset
-    console.log(dayjs().month(dayjs().month() + monthChange).date(1))
-    console.log(currentYear);
-    console.log(dayjs().year())
-
     // get the dayjs() object of the first day of the current month, ex. "December 1 2020". Modify by the monthChange offset.
     let firstDate = dayjs().month(dayjs().month() + monthChange).date(1)
+    setCurrentMonth(firstDate.format('MMMM'))
+    setCurrentYear(firstDate.format("YYYY"))
     // finally, get the day the the first day of the month was on, ex "Tuesday"
     let firstDay = dayjs(firstDate).format('dddd')
 
@@ -34,18 +27,17 @@ export default function MiniCalendar() {
     // get number of days in the current month
     // the below is equiv to ex. dayjs("2020-12-01").daysInMonth() 
     let daysInMonth = firstDate.daysInMonth()
-    console.log(daysInMonth)
 
     // calculate the number of weeks in a month. Then make a new array of that length.
     let numWeeksArr = new Array(Math.ceil((daysInMonth + dayIndex) / 7)).fill(0)
-    console.log()
-    console.log(numWeeksArr)
     let firstWeek = true;
     let secondWeek = true;
     // use dateNum to keep track of date on miniCalendar
     let dateNum = 1;
+    // map out the rows of weeks. Each MiniDay.js component is a table row, consisting of a week.
     setMiniDays(
       <table>
+        <tbody>
         {numWeeksArr.map(a => {
           if (firstWeek) {
             // now dateNum is the first day of the second week, ex. Dec. 6
@@ -53,6 +45,7 @@ export default function MiniCalendar() {
             firstWeek = false;
             return(
               <MiniDay
+                key={dateNum}
                 firstWeek={true}
                 dayIndex={dayIndex}
               >
@@ -64,10 +57,12 @@ export default function MiniCalendar() {
               secondWeek = false;
             }
             else {
+              // increase the date number by 7 every week after the second
               dateNum += 7;
             }
             return (
               <MiniDay
+                key={dateNum + 1}
                 daysInMonth={daysInMonth}
                 firstWeek={false}
                 dateNum={dateNum}
@@ -76,22 +71,22 @@ export default function MiniCalendar() {
             )
           }
         })}
+        </tbody>
       </table>)
     console.log(miniDays)
   }, [monthChange])
 
   const backMonth = () => {
-    console.log('back clicked')
     setMonthChange(monthChange - 1)
   }
   
   const forwardMonth = () => {
-    console.log('forward')
     setMonthChange(monthChange + 1)
   }
 
   return (<>
   <button className='btn btn-secondary' onClick={backMonth}>back month</button>
+  <p>{currentMonth}, {currentYear}</p>
     {miniDays}
   <button className='btn btn-secondary' onClick={forwardMonth}>forward month</button>
   </>)
